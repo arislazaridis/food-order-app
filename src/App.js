b9fca0c9dbdfd8c4b7bc2c2df30b11aeeacdef7d
arislazaridis <arislazaridis@yahoo.com>
2021-12-02 15:27:23 +0200

    food-order-app

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from "./logo.svg";
+import "./App.css";
+import React, { Fragment } from "react";
+import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
+
+function App() {
+  return (
+    <Fragment>
+      <Header />
+      <main>
+        <Meals />
+      </main>
+    </Fragment>
+  );
+}
+
+export default App;
