088f8aef7872ba3736f6f95117728b028b122313
arislazaridis <arislazaridis@yahoo.com>
2022-03-29 15:17:52 +0300

    Fix input range

--- src/components/Meals/MealItemForm.js
+++ src/components/Meals/MealItemForm.js
@@ -26,7 +26,7 @@ function MealItemForm(props) {
         input={{
           id: "amount",
           type: "number",
-          min: "1",
+          min: "0",
           max: "5",
           step: "1",
           defaultValue: "1",
@@ -39,7 +39,6 @@ function MealItemForm(props) {
       >
         + Add
       </button>
-      <button type="submit">- Delete</button>
     </form>
   );
 }
